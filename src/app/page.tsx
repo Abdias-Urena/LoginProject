3930ecdb7ab36ff4270a13a27e8bc212fe509d58
Abdías Ureña <abdias.urena.soto@est.una.ac.cr>
2023-05-23 22:50:20 -0600

    Se agrega gif y seccion del Inicio de Sesion y se modifica el page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
+import SignUpSection from '@/components/SignUpSection'
 export default function Home() {
   return (
     <div>
-
+      <SignUpSection/>
     </div>
   )
 }
