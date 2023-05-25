b630134d5e5ca1a8045d4f3ac6928636fe5bac3a
sebasaravi <sebastian.arias.aviles@est.una.ac.cr>
2023-05-24 20:09:45 -0600

    Add a 3 new components with social media and to change a name a file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
+import SignInSection from '@/components/SignInSection'
 import SignUpSection from '@/components/SignUpSection'
+import ForgotPassword from '@/components/ForgotPassword'
+import ResetPassword from '@/components/ResetPassword'
 export default function Home() {
   return (
     <div>
